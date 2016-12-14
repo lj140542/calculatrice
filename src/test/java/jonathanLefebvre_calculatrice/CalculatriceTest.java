2f91a56f5f998b713a7cb9ca587e320e9378c16f
Jonathan <Jonathan@172.30.11.155>
2016-12-14 08:53:56 +0100

    1.0.1 / Ajouts de quelques tests en plus
    * Ajout de tests sur les multiplications par 0 et double negatif
    * Ajout de tests sur la soustraction par un double negatif
    * Ajout de tests sur les puissances de 0, 1, et double negatif

--- src/test/java/jonathanLefebvre_calculatrice/CalculatriceTest.java
+++ src/test/java/jonathanLefebvre_calculatrice/CalculatriceTest.java
@@ -21,6 +21,10 @@ public class CalculatriceTest {
 		calculatrice = new Calculatrice();
 	}
 
+	// Les opérandes étant toutes des doubles, et le nombre de celles-ci étant
+	// vérifié avant l'appel de chacune des méthodes de calculs, Les paramètres
+	// ne peuvent êtres null ou en dehors de l'ensemble sur lequel le calcul se
+	// réalise
 	@Test
 	public void testAddition() {
 		assertEquals(5, calculatrice.addition(3, 2), 0.00001);
@@ -30,10 +34,25 @@ public class CalculatriceTest {
 	public void testSoustraction() {
 		assertEquals(5, calculatrice.soustraction(7, 2), 0.00001);
 	}
+	
+	@Test
+	public void testSoustractionNegative() {
+		assertEquals(5, calculatrice.soustraction(3, -2), 0.00001);
+	}
 
 	@Test
 	public void testMultiplication() {
 		assertEquals(6, calculatrice.multiplication(3, 2), 0.00001);
+	}
+	
+	@Test
+	public void testMultiplicationNulle() {
+		assertEquals(0, calculatrice.multiplication(3, 0), 0.00001);
+	}
+	
+	@Test
+	public void testMultiplicationNegative() {
+		assertEquals(-9, calculatrice.multiplication(3, -3), 0.00001);
 	}
 
 	@Test
@@ -60,10 +79,10 @@ public class CalculatriceTest {
 	public void testCos() {
 		assertEquals(0.5, calculatrice.cos(Math.PI / 3), 0.00001);
 	}
-	
+
 	@Test
 	public void testTan() {
-		assertEquals(1.73205080757, calculatrice.tan(Math.PI/3), 0.00001);
+		assertEquals(1.73205080757, calculatrice.tan(Math.PI / 3), 0.00001);
 	}
 
 	@Test
@@ -75,17 +94,29 @@ public class CalculatriceTest {
 	public void testOpp() {
 		assertEquals(-5, calculatrice.opp(5), 0.00001);
 	}
-	
+
 	@Test
 	public void testPuiss() {
 		assertEquals(8, calculatrice.puiss(2, 3), 0.00001);
+	}
+
+	@Test
+	public void testPuissNull() {
 		assertEquals(1, calculatrice.puiss(2, 0), 0.00001);
+	}
+
+	@Test
+	public void testPuissUn() {
 		assertEquals(2, calculatrice.puiss(2, 1), 0.00001);
+	}
+
+	@Test
+	public void testPuissNegatif() {
 		assertEquals(0.25, calculatrice.puiss(2, -2), 0.00001);
 	}
-	
+
 	@Test
 	public void testGamma() {
-		assertEquals(2.67893853 , calculatrice.gamma(1.0/3), 0.00001);
+		assertEquals(2.67893853, calculatrice.gamma(1.0 / 3), 0.00001);
 	}
 }
